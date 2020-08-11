a4ab23ced01b87a551f829fa7c9e61cf0b38d519
ccianos <ccianos@alphavida.com>
2020-08-11 01:25:09 -0700

    adding indexFromZeroShortcut.nim.

---
+++ indexFromZeroShortcut.nim
@@ -0,0 +1,12 @@
+type
+  IntArray = array[0..5, int]
+  QuickArray = array[6, int]
+
+var
+  x: IntArray
+  y: QuickArray
+x = [1, 2, 3, 4, 5, 6]
+y = x
+
+for i in low(x)..high(x):
+  echo x[i], y[i]
