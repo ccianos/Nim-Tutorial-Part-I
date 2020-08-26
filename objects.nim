ce5d399178b7793fc6e0864915865e28161329d8
ccianos <ccianos@alphavida.com>
2020-08-26 01:06:38 -0700

    Add to objects.nim

--- objects.nim
+++ objects.nim
@@ -7,3 +7,17 @@ var person1 = Person(name: "Peter", age: 30)
 
 echo person1.name
 echo person1.age
+
+var person2 = person1
+person2.age += 15
+
+doAssert person1.age < person2.age
+echo person1.age 
+echo person2.age
+
+let person3 = Person(age: 26, name: "Miguel")
+echo person3
+
+let person4 = Person(age: 8)
+doAssert person4.name == ""
+doAssert person4.age == 8
